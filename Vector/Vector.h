1f0f75223bf5745cf4fe7a214ea9952722acf28e
simplydesigner <simplydesigner@ya.ru>
2016-02-17 09:53:11 +0300

    Initial Commit

---
+++ Vector/Vector.h
@@ -0,0 +1,19 @@
+//
+//  Vector.h
+//  Vector
+//
+//  Created by simplydesigner on 2/17/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Vector.
+FOUNDATION_EXPORT double VectorVersionNumber;
+
+//! Project version string for Vector.
+FOUNDATION_EXPORT const unsigned char VectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vector/PublicHeader.h>
+
+
